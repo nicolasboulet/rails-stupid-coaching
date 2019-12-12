84747f79b19bd694dde981d4c70333949971b360
Nicolas Boulet <nicolasboulet@live.fr>
2019-12-12 20:16:08 +0100

    Supid coach

---
+++ test/system/questions_test.rb
@@ -0,0 +1,35 @@
+require 'application_system_test_case'
+
+class QuestionsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit questions_url
+  #
+  #   assert_selector "h1", text: "Questions"
+  # end
+  test 'visiting /ask renders the form' do
+    visit ask_url
+    assert_selector 'p', text: 'Ask your question ?'
+  end
+
+  test 'saying Hello yields a grumpy response from the coach' do
+    visit ask_url
+    fill_in 'question', with: 'Hello'
+    click_on 'Ask'
+    assert_text "I don't care, get dressed and go to work!"
+  end
+
+  test 'saying a question' do
+    visit ask_url
+    fill_in 'question', with: 'Hello?'
+    click_on 'Ask'
+    assert_text 'Silly question, get dressed and go to work!'
+  end
+
+  test 'saying i will work' do
+    visit ask_url
+    fill_in 'question', with: 'I am going to work'
+    click_on 'Ask'
+    assert_text 'Great!'
+    take_screenshot
+  end
+end
